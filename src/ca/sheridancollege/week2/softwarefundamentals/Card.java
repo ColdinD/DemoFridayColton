89cb63f8d1cdd5f26f37c775644b2dca8ddcbef0
ColdinD <46826722+ColdinD@users.noreply.github.com>
2019-02-01 13:02:48 -0500

    Update Card.java

--- src/ca/sheridancollege/week2/softwarefundamentals/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/Card.java
@@ -16,7 +16,8 @@ public class Card
 {
     private String suit;// hearts, diamonds, spades and clubs
     private int value;//1-13
-    
+    int a;//new variable
+    a=10;
     /**
      * A constructor that creates a Card Object with a given
      * suit and a given value.
